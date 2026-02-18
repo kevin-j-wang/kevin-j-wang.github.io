cb1967f7759296e1ed94ed144ad91ceb84ae73f8
Kevin Wang <kevinj.wang@mail.utoronto.ca>
2026-02-17 19:48:15 -0500

    testing updating site

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMRasdasdsa
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Under Construction
       </p>
     </>
   )
